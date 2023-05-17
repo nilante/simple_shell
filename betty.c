05c793f8c454cbe16bdf8176cb37aaa96da02817
marienachat <mariem.nachaat@gmail.com>
2023-05-17 13:16:37 -0700

    q

--- betty.c
+++ betty.c
@@ -1,17 +1,17 @@
 #include "main.h"
 int main(void)
 {
-    char *line;
-    char **args;
+char *l;
+char **ar;
 
-    while (1) {
-        printf("> ");
-        line = read_line();  // Read a line of input from the user
-        args = split_line(line);  // Split the line into arguments
-        execute_command(args);  // Execute the command
-        free(line);
-        free(args);
-    }
-
-    return 0;
+while (1)
+{
+printf("> ");
+line = read_line();  /* Read a line of input from the user */
+ars = split_line(line);  /* Split the line into arguments */
+execute_command(args);  /* Execute the command */
+free(l);
+free(ar);
+}
+return (0);
 }
--- env.c
+++ env.c
@@ -1,31 +1,41 @@
 #include "main.h"
 void execute_command(char **args)
 {
-    if (args[0] == NULL) {
-        // Empty command
-        return;
-    }
-
-    if (strcmp(args[0], "env") == 0) {
-        // Handle the "env" built-in command
-        env_builtin();
-    } else {
-        // Handle external commands
-        pid_t pid = fork();
-
-        if (pid == 0) {
-            // Child process
-            if (execvp(args[0], args) == -1) {
-                perror("execvp");
-            }
-            exit(EXIT_FAILURE);
-        } else if (pid < 0) {
-            // Forking error
-            perror("fork");
-        } else {
-            // Parent process
-            wait(NULL);
-        }
-    }
+if (args[0] == NULL)
+{
+/**
+*  Empty command
+*/
+return;
 }
+if (strcmp(args[0], "env") == 0) 
+{
+/* Handle the "env" built-in command */
+env_builtin();
+}
+else
+{
+/* Handle external commands */
+pid_t pid = fork();
 
+if (pid == 0)
+{
+/* Child process */
+if (execvp(args[0], args) == -1)
+{
+perror("execvp");
+}
+exit(EXIT_FAILURE);
+}
+else if (pid < 0)
+{
+/* Forking error */
+perror("fork");
+}
+else
+{
+/* Parent process */
+wait(NULL);
+}
+}
+}
